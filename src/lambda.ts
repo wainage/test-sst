6df0d35ba9946618e8c20e9770dcf25d9eba80c6
Wayne Muller <github@wainage.com>
2021-11-11 21:23:32 -0500

    Initial commit

---
+++ src/lambda.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Clown world from shitty docs ... ${event.requestContext.time}.`,
+  };
+};
